a4f3123eb02ed34d4106a20f1021e0f66ee98c15
Matteo Cropelli <matteo.cropelli01@gmail.com>
2025-11-27 11:40:28 +0100

    setup React + Vite (installed React, React DOM, Axios, React Router

---
+++ resources/js/app.jsx
@@ -0,0 +1,14 @@
+import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (
+        <h1>
+            Prova
+        </h1>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
